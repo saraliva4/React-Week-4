520bd3087b6aa7f38e773a156136c7ee83f13aa7
saraliva4 <122287923+saraliva4@users.noreply.github.com>
2026-04-09 10:51:19 +0200

    changed the searchbar to fixed position

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import axios from "axios";
-import Button from "react-bootstrap/Button";
 import "./Search.css";
 
 export default function Search() {
   return (
-    <div className="Search">
-      <form id="search-form">
-        <span>
-          <i className="fa-solid fa-magnifying-glass search-icon search"></i>
-        </span>
-        <input
-          className="search search-bar"
-          type="text"
-          id="search-input"
-          placeholder="Search"
-        />
-        <button
-          className="search current-location-button"
-          id="current-location-button"
-        >
-          Current location
-        </button>
-      </form>
+    <div className="Search m-4">
+      <div className="start-50 translate-middle search-bar">
+        <div>
+          <form id="search-form">
+            <span>
+              <i className="fa-solid fa-magnifying-glass search-icon"></i>
+            </span>
+            <input
+              className=" search-input"
+              type="text"
+              id="search-input"
+              placeholder="Type a city..."
+            />
+            <input type="submit" value="Search" className="search-button" />
+            <button
+              className="current-location-button"
+              id="current-location-button"
+            >
+              Current location
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
