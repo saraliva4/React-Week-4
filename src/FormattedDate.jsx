54fe3d18c72d066e12f3664a087a618229b99fd7
saraliva4 <122287923+saraliva4@users.noreply.github.com>
2026-04-17 18:02:14 +0200

    Changed the format for the minutes and hours

--- src/FormattedDate.jsx
+++ src/FormattedDate.jsx
@@ -27,15 +27,21 @@ export default function FormattedDate(props) {
   let month = months[props.date.getMonth()];
   let date = props.date.getDate();
   let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
   return (
     <div className="FormattedDate">
       <div>
-        {month} {date},
+        {day} {hours}:{minutes},
       </div>
       <div>
-        {day} {hours}:{minutes}
+        {month} {date}
       </div>
     </div>
   );
